1dda633876900c0b5639023c2591183269f442c0
grahamlyons <graham@grahamlyons.com>
2019-09-09 13:12:32 +0100

    Add a Dockerfile and test script to run tests
    There are some lovely tests for the Python script so this adds a
    way for everyone to run them easily. The test for the Terraform
    code is simply that it passes validation.

---
+++ test.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+set -euo pipefail
+
+IMAGE_ID="$(basename $(pwd))"
+
+function cleanup() {
+    docker container rm --force "${IMAGE_ID}"
+}
+
+function run_in_container() {
+    docker container exec -i \
+        "$(tty -s && echo -t)" \
+        "${IMAGE_ID}" \
+        "${@}"
+}
+
+trap cleanup EXIT
+
+docker image build -t "${IMAGE_ID}" .
+
+docker container run \
+    --name "${IMAGE_ID}" \
+    --entrypoint= \
+    -d \
+    "${IMAGE_ID}" \
+    tail -f /dev/null
+
+run_in_container terraform init
+run_in_container terraform validate -var plan_name=myplan -var database=mydb
+run_in_container python3 -m unittest discover scripts
